d7f283cdf47d079ee9b5283c67fe4bb9a4550e8c
TICOM Webcomic <hashraygoza@protonmail.com>
2021-02-18 00:23:45 -0600

    1.3
    Limpieza de código

--- src/main/java/mx/hash/xml2json/ConversorJSON.java
+++ src/main/java/mx/hash/xml2json/ConversorJSON.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mx.hash.xml2json;
 
 import java.io.File;
@@ -22,46 +17,38 @@ import org.json.XML;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
-/**
- *
- * @author draygoza
- */
 public class ConversorJSON {
+
     public String convertir(String file) throws ParserConfigurationException, SAXException, IOException, TransformerException, TransformerConfigurationException {
-        // Generador de constructor de objetos XML
-            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        File archivo = new File(file);
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
 
-            // Esto es para agilizar la lectura de archivos grandes
-            documentBuilderFactory.setNamespaceAware(false);
-            documentBuilderFactory.setValidating(false);
-            documentBuilderFactory.setFeature("http://xml.org/sax/features/namespaces", false);
-            documentBuilderFactory.setFeature("http://xml.org/sax/features/validation", false);
-            documentBuilderFactory.setFeature("http://apache.org/xml/features/nonvalidating/load-dtd-grammar", false);
-            documentBuilderFactory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+        // Esto es para agilizar la lectura de archivos grandes
+        documentBuilderFactory.setNamespaceAware(false);
+        documentBuilderFactory.setValidating(false);
+        documentBuilderFactory.setFeature("http://xml.org/sax/features/namespaces", false);
+        documentBuilderFactory.setFeature("http://xml.org/sax/features/validation", false);
+        documentBuilderFactory.setFeature("http://apache.org/xml/features/nonvalidating/load-dtd-grammar", false);
+        documentBuilderFactory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
 
-            // constructor de objetos XML
-            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();            
+        // constructor de objetos XML
+        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
 
-            // Ruta del archivo XML
-            File archivo = new File(file);
+        Document documento = documentBuilder.parse(archivo);
 
-            // Objeto Documento XML
-            Document documento = documentBuilder.parse(archivo);
-            
-            // Esto ayuda al procesamiento
-            documento.getDocumentElement().normalize();
+        // Esto ayuda al procesamiento
+        documento.getDocumentElement().normalize();
 
-            StringWriter writer = new StringWriter();
-            TransformerFactory tf = TransformerFactory.newInstance();
-            Transformer transformer = tf.newTransformer();
-            transformer.transform(new DOMSource(documento), new StreamResult(writer));
+        StringWriter writer = new StringWriter();
+        TransformerFactory tf = TransformerFactory.newInstance();
+        Transformer transformer = tf.newTransformer();
+        transformer.transform(new DOMSource(documento), new StreamResult(writer));
 
-            String xmlString = writer.getBuffer().toString();
-            
-            JSONObject jsonObj = XML.toJSONObject(xmlString);
-            String json = jsonObj.toString(4);
-            
-            return json;
+        String xmlString = writer.getBuffer().toString();
+
+        JSONObject jsonObj = XML.toJSONObject(xmlString);
+        String json = jsonObj.toString(4);
+
+        return json;
     }
-    
 }
